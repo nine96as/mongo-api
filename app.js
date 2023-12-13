6e6b035e9027fe981f0ec1c8066121dc4e90b9ee
nine96as <nine96@skiff.com>
2023-12-13 18:24:45 +0000

    Add router refs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import logger from 'morgan';
+import booksRouter from './routers/books.js';
 
 export const app = express();
 
@@ -8,9 +9,12 @@ app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
 
+app.use('/books', booksRouter);
+
 app.get('/', (req, res) => {
   res.status(200).send({
-    message: 'Welcome!',
-    description: 'mongoAPI'
+    message: 'Books API',
+    description: 'TBC',
+    endpoints: [['POST  /books']]
   });
 });
