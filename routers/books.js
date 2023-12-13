d4bd1e6e5a971f52d2647f6b5527f07f7a5e46e9
nine96as <nine96@skiff.com>
2023-12-13 20:36:15 +0000

    Add update() refs with patch req

--- routers/books.js
+++ routers/books.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { create, index, show } from '../controllers/books.js';
+import { create, index, show, update } from '../controllers/books.js';
 
 const booksRouter = Router();
 
 booksRouter.get('/', index);
 booksRouter.post('/', create);
 booksRouter.get('/:id', show);
+booksRouter.patch('/:id', update);
 
 export default booksRouter;
