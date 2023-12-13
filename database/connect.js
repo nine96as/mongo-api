9767d6d06c0658aeafdd58e4e8971b97b693838f
nine96as <nine96@skiff.com>
2023-12-13 18:15:06 +0000

    Combine db connect logic in index.js file, simplify dev script

--- database/connect.js
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config';
-import mongoose from 'mongoose';
-
-mongoose
-  .connect(process.env.DB_URL)
-  .then(() => console.log('API connected to DB'))
-  .catch((e) => console.log(e));
