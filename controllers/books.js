04218de3c3987200e6c18892c8b01f526ccd2c31
nine96as <ashrafsaid79@gmail.com>
2023-12-13 18:42:10 +0000

    Implement show() logic

--- controllers/books.js
+++ controllers/books.js
@@ -20,14 +20,23 @@ export const create = async (req, res) => {
         error: 'You must send all required fields: title, author, publishYear'
       });
 
-    const book = await Book.create({
-      title: title,
-      author: author,
-      publishYear: publishYear
-    });
-
-    return res.status(201).json(book);
+    return res.status(201).json(
+      await Book.create({
+        title: title,
+        author: author,
+        publishYear: publishYear
+      })
+    );
   } catch (e) {
     res.status(500).json({ error: e.message });
+  }
+};
+
+export const show = async (req, res) => {
+  try {
+    const { id } = req.params;
+    res.status(200).json(await Book.findById(id));
+  } catch (e) {
+    res.status(404).send({ error: e.message });
   }
 };
