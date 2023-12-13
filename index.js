e295f2269522989ca0f27deb19e777d8d1fae9f7
nine96as <nine96@skiff.com>
2023-12-13 17:37:55 +0000

    Add initial api skeleton

---
+++ index.js
@@ -0,0 +1,7 @@
+import 'dotenv/config';
+import { app } from './app.js';
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server running on port: http://localhost:${port}`);
+});
